bb643eb24be0060786e37d427d499e24b9e32179
ReanGD <reangd@yandex.ru>
2016-02-14 02:08:08 +0300

    added the ability to compile in a different directory

--- multi-compile.el
+++ multi-compile.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Kvashnin Vladimir <reangd@gmail.com>
 ;; Created: 2015-10-01
-;; Version: 0.3.0
+;; Version: 0.4.0
 ;; Keywords: tools compile build
 ;; URL: https://github.com/ReanGD/emacs-multi-compile
 ;; Package-Requires: ((emacs "24"))
@@ -85,7 +85,6 @@
 ;; https://github.com/ReanGD/emacs-multi-compile/blob/master/README.md
 ;;
 ;;; Code:
-
 (require 'compile)
 
 (defgroup multi-compile nil
@@ -100,19 +99,23 @@
                   ("rust-release" . "cargo run --release")
                   ("rust-test" . "cargo test")))
     (c++-mode . (("cpp-run" . "make --no-print-directory -C %make-dir")))
-    ("\\.txt\\'" . (("text-filename" . "echo %file-name")))
     )
   "Alist of filename patterns vs corresponding format control strings."
   :type '(repeat
           (cons
-           (choice
-            (regexp :tag "Filename pattern")
-            (function :tag "Major-mode"))
-           (repeat
-            (cons
-             (string :tag "Compilation name")
-             (string :tag "Compilation command"))
-            )))
+           (choice :tag "Key"
+                   (regexp :tag "Filename or buffer pattern")
+                   (function :tag "Major-mode"))
+           (repeat :tag "Settings"
+                   (choice :tag "Type"
+                           (cons :tag "Default compilation directory"
+                                 (string :tag "Menu item")
+                                 (string :tag "Command"))
+                           (list :tag "Set compilation directory"
+                                 (string :tag "Menu item")
+                                 (string :tag "Command")
+                                 (function :tag "Function returns a compilation root"))
+                           ))))
   :group 'multi-compile)
 
 (defcustom multi-compile-template
@@ -138,7 +141,7 @@
   :group 'multi-compile)
 
 
-(defun multi-compile--apply-template (format-string)
+(defun multi-compile--fill-template (format-string)
   (dolist (template multi-compile-template)
     (while (string-match (car template) format-string)
       (let ((new-text (save-match-data (eval (cdr template)))))
@@ -188,28 +191,29 @@
          (t (funcall multi-compile-completion-system prompt choices)))
         compile-list)))))
 
-(defun multi-compile--user-command (command)
-  "Read custom compile command"
-  (interactive "sCompile command: ")
-  (compilation-start command))
+(defun multi-compile--get-command-template ()
+  (let ((filename (if (stringp buffer-file-name) buffer-file-name (buffer-name))))
+    (if (not filename)
+        (read-input "Compile command: ")
+      (let ((command-list (multi-compile--fill-command-list filename)))
+        (if command-list
+            (multi-compile--choice-compile-command command-list)
+          (read-input "Compile command: "))))))
 
+;;;###autoload
+(defun multi-compile-locate-file-dir (name)
+  (lambda ()
+    (locate-dominating-file (buffer-file-name) name)))
 
 ;;;###autoload
 (defun multi-compile-run ()
   "Choice target and start compile."
   (interactive)
-  (let ((filename (if (stringp buffer-file-name) buffer-file-name (buffer-name)))
-        (command-list '()))
-    (if (not filename)
-        (call-interactively 'multi-compile--user-command)
-      (progn
-        (setq command-list (multi-compile--fill-command-list filename))
-        (if command-list
-            (compilation-start
-             (multi-compile--apply-template
-              (multi-compile--choice-compile-command command-list)))
-          (call-interactively 'multi-compile--user-command)
-          )))))
+  (let* ((template (multi-compile--get-command-template))
+         (command (or (car-safe template) template))
+         (default-directory (if (listp template) (funcall (cadr template)) default-directory)))
+    (compilation-start
+     (multi-compile--fill-template command))))
 
 (provide 'multi-compile)
 
